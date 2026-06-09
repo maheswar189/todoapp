23694f39bfddb3ae737f110e1771d3b422bd9c55
maheswar189 <maheswarreddy189@gmai.com>
2026-06-09 07:10:18 +0530

    ReactJS Tutorial 35: Todo App Part 8 CSS Modules For Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Todo from "./components/Todo";
 import InlineComponent from "./components/InlineComponent";
+import OutlineComponent from "./components/OutlineComponent";
 function App() {
   return (
     <div className="App">
       {/* <Todo /> */}
       <InlineComponent />
+      <OutlineComponent />
     </div>
   );
 }
--- src/components/InlineComponent.jsx
+++ src/components/InlineComponent.jsx
@@ -1,9 +1,10 @@
-import "../css/style.css";
+//import "../css/style.css";
+import styles from "./inlinecomponet.module.css";
 
 export default function InlineComponent() {
   return (
     <div>
-      <h1 className="header">Inline Component</h1>
+      <h1 className={styles.header}>Inline Component</h1>
     </div>
   );
 }
