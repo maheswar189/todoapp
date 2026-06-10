75729f347e88105341fbaca6e2cb9c8d4abf5969
maheswar189 <maheswarreddy189@gmai.com>
2026-06-10 06:56:31 +0530

    ReactJS Tutorial 37: Todo App Part 10 Separating JSX Into Components

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,26 +1,13 @@
 import { useState } from "react";
 import TodoItem from "./TodoItem";
+import Form from "./Form";
+import TodoList from "./TodoList";
 export default function Todo() {
-  const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
-  function handleSubmit(e) {
-    e.preventDefault();
-    setTodos([...todos, todo]);
-    setTodo("");
-  }
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={todo}
-          onChange={(e) => setTodo(e.target.value)}
-        />
-        <button type="submit">Add</button>
-      </form>
-      {todos.map((item) => (
-        <TodoItem key={item} item={item} />
-      ))}
+      <Form todos={todos} setTodos={setTodos} />
+      <TodoList todos={todos} />
     </div>
   );
 }
