da5e1ddc6b83710a5fb79591b9cd96f7f0fc419a
maheswar189 <maheswarreddy189@gmai.com>
2026-06-14 15:54:16 +0530

    ReactJS Tutorial 41: Todo App Part 14 Delete Functionality In React

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,11 +1,20 @@
 import styles from "./todoItem.module.css";
-export default function TodoItem({ item }) {
+export default function TodoItem({ item, todos, setTodos }) {
+  function handleDelete(itemToDelete) {
+    console.log("Delete item:", item);
+    setTodos(todos.filter((todo) => todo !== itemToDelete));
+  }
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
         {item}
         <span>
-          <button className={styles.deleteButton}>X</button>
+          <button
+            onClick={() => handleDelete(item)}
+            className={styles.deleteButton}
+          >
+            X
+          </button>
         </span>
       </div>
 
